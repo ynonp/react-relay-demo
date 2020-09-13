56dd56f93932782f52ed96430f7b023e4d73d5dc
ynonp <ynonperek@gmail.com>
2020-09-13 14:40:39 +0300

    expand tab

--- src/App.js
+++ src/App.js
@@ -5,52 +5,52 @@ import graphql from 'babel-plugin-relay/macro';
 import environment from './environment';
 
 function UserData(props) {
-	const { login, user } = props;
-	if (!user) {
-		return <div>Loading...</div>;
-	}
-	return (
-		<p>User {login} has {user.repositories.totalCount} repos</p>
-	);
+  const { login, user } = props;
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <p>User {login} has {user.repositories.totalCount} repos</p>
+  );
 }
 
 
 export default function App(props) {
-	const [login, setLogin] = useState('ynonp');
-	const loginInput = useRef(null);
+  const [login, setLogin] = useState('ynonp');
+  const loginInput = useRef(null);
 
-	function getRepoCount(e) {
-		e.preventDefault();
-		setLogin(loginInput.current.value);
-	}
+  function getRepoCount(e) {
+    e.preventDefault();
+    setLogin(loginInput.current.value);
+  }
 
-	return (
-		<QueryRenderer
-			environment={environment}
-			query={graphql`
-					query AppQuery($login: String!) {
-						user(login: $login) { 
-							repositories {
-									totalCount
-							}
-						}
-					}
-				`}
-					variables={{login}}
-					render={({error, props}) => {
-						if (error) {
-							return <div>Error!</div>;
-						}
-						return (
-							<div>
-								<form onSubmit={getRepoCount}>
-									<input type="text" defaultValue={login} ref={loginInput} />
-									<button>Get Repo Count</button>
-								</form>
-								<UserData {...props} login={login} />
-							</div>
-						);
-					}}
-		/>
-	);
+  return (
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+          query AppQuery($login: String!) {
+            user(login: $login) { 
+              repositories {
+                  totalCount
+              }
+            }
+          }
+        `}
+          variables={{login}}
+          render={({error, props}) => {
+            if (error) {
+              return <div>Error!</div>;
+            }
+            return (
+              <div>
+                <form onSubmit={getRepoCount}>
+                  <input type="text" defaultValue={login} ref={loginInput} />
+                  <button>Get Repo Count</button>
+                </form>
+                <UserData {...props} login={login} />
+              </div>
+            );
+          }}
+    />
+  );
 }
